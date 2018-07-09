480899bf0130b59b4c746c8c63110963dac7422e
jacoloves <5511068t@gmail.com>
2018-07-10 00:33:55 +0900

    20180710_cording

--- html/lesson2/php_lesson2_8.php
+++ html/lesson2/php_lesson2_8.php
@@ -6,10 +6,44 @@ $menus = array(
 );
 
 // この下にコードを書いてください
+$cnt = 0;
+$sum = 0;
+$maxprice = 0;
+$foodi = '';
+$num = 0;
+$flg = 0;
 foreach ($menus as $menu) {
     foreach($menu as $micro){
-	foeach()
+        $num++;
+	if($cnt == 0){
+	    echo $micro.'は';
+	}elseif($cnt == 1){
+	    echo $micro.'円です';
+            $sum += $micro;
+	    if($maxprice < $micro){
+		$maxprice = $micro;
+                $flg = $num - 1;
+	    }
+	}
+	$cnt++;
+    }
+    $cnt = 0;
+    echo '<br>';
+}
+
+$num = 0;
+
+foreach($menus as $menu){
+    foreach($menu as $foods){
+	$num++;
+	if($num == $flg){
+            $food = $foods;
+        }
     }
 }
+
+echo "合計金額は{$sum}円です";
+echo '<br>';
+echo "{$food}が最高価格で{$maxprice}円です";
 
 ?>
