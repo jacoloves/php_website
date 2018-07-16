5c31accce0c281d0e4bfa1bdc6cf07bfdd3b1aff
Shotaro Tanaka <5511068t@gmail.com>
2018-07-16 23:56:54 +0900

    20180716

---
+++ phpstudy/bookapp/app/Http/routes.php
@@ -0,0 +1,34 @@
+<?php
+
+use App/Book;
+use Illuminate/Http/Request;
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', function () {
+    return view('books');
+});
+
+Route::group(['middleware' => ['web']], function () {
+
+    Route::get('/', function(){
+        //
+    });
+
+    Route::post('/book',function(Request $request){
+        //
+    });
+
+    Route::delete('/book/{book}', function(Book $book){
+        //
+    });
+});
