7e2079e5cb87bc94abaa61bc255001d8b5fd2346
Shotaro Tanaka <5511068t@gmail.com>
2018-09-28 07:00:29 +0900

    20180928

--- laravelapp2/app/Http/Middleware/HelloMiddleware.php
+++ laravelapp2/app/Http/Middleware/HelloMiddleware.php
@@ -15,13 +15,14 @@ class HelloMiddleware
      */
     public function handle($request, Closure $next)
     {
-        $data = [
-            ['name'=>'taro', 'mail'=>'taro@yamada'],
-            ['name'=>'hanako', 'mail'=>'hanako@flower'],
-            ['name'=>'sachiko', 'mail'=>'sachiko@happy'],
-        ];
+        $responce = $next($request);
+        $content = $responce->content();
 
-        $request->merge(['data'=>$data]);
-        return $next($request);
+        $pattern = '/<middleware>(.*)<\/middleware>/i';
+        $replace = '<a href="http://$1">$1</a>';
+        $content = preg_replace($pattern, $replace, $content);
+
+        $response->setContent($content);
+        return $response;
     }
 }
