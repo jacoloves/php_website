c9b4a4684ae456e07e17f031a8ad5b9d90390245
Shotaro Tanaka <5511068t@gmail.com>
2018-10-01 00:15:18 +0900

    20191001

--- laravelapp2/app/Http/Controllers/HelloController.php
+++ laravelapp2/app/Http/Controllers/HelloController.php
@@ -11,21 +11,12 @@ class HelloController extends Controller
 {
     public function index(Request $request)
     {
-        return view('hello.index', ['msg'=>'フォームを入力：']);
+        return view('hello.index', ['msg'=>'フォームを入力して下さい。']);
     }
 
-    public function post(Request $requst)
+    public function post(HelloRequest $request)
     {
-        $validator = Validator::make($request->query(), [
-            'id' => 'required',
-            'pass' => 'required',
-        ]);
-        if($validator->fails()){
-            $msg = 'クエリーに問題があります。';
-        }else{
-            $msg = 'ID/PASSを受け付けました。フォームを入力下さい。';
-        }
-
-        return view('hello.index', ['msg'=>$msg, ]);
+        return view('hello.index', ['msg'=>'正しく入力されました！']);
+        
     }
 }
--- laravelapp2/app/Providers/HelloServiceProvider.php
+++ laravelapp2/app/Providers/HelloServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
+use Illuminata\Validation\Validator;
+use App\Http\Validators\HelloValidator;
 
 class HelloServiceProvider extends ServiceProvider
 {
@@ -14,8 +16,8 @@ class HelloServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        View::composer(
-            'hello.index', 'App\Http\Composers\HelloComposer'
-        );
+        Validator::extend('hello', function($arrtibute, $value, $parameters, $validator){
+            return $value % 2 == 0;
+        });
     }
 }
