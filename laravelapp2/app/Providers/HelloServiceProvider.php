8b51e4e2e8d5f8f971932970c5064d4d4e2d57d8
Shotaro Tanaka <5511068t@gmail.com>
2018-10-01 23:08:42 +0900

    20181001

--- laravelapp2/app/Providers/HelloServiceProvider.php
+++ laravelapp2/app/Providers/HelloServiceProvider.php
@@ -4,7 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
-use Illuminata\Validation\Validator;
+use Illuminate\Validation\Validator;
 use App\Http\Validators\HelloValidator;
 
 class HelloServiceProvider extends ServiceProvider
@@ -16,8 +16,9 @@ class HelloServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Validator::extend('hello', function($arrtibute, $value, $parameters, $validator){
-            return $value % 2 == 0;
+        $validator = $this->app['validator'];
+        $validator->resolver(function($translator, $data, $rules, $messages){
+            return new HelloValidator($translator, $data, $rules, $messages);
         });
     }
 }
