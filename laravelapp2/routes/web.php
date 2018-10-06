fde40276b421534c53dc3f305fb7e73e7f4185fa
Shotaro Tanaka <5511068t@gmail.com>
2018-10-06 18:39:12 +0900

    20181006

--- laravelapp2/routes/web.php
+++ laravelapp2/routes/web.php
@@ -15,6 +15,11 @@ use App\Http\Middleware\HelloMiddleware;
 
 Route::get('hello', 'HelloController@index');
 Route::post('hello', 'HelloController@post');
+Route::get('hello/add', 'HelloController@add');
+Route::post('hello/add', 'HelloController@create');
+Route::get('hello/edit', 'HelloController@edit');
+Route::post('hello/edit', 'HelloController@update');
+
 
 /*
 Route::get('/', function () {
