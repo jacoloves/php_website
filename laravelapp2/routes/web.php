4e36a2b8d5fefd09eef5460e9b764819e66242ee
Shotaro Tanaka <5511068t@gmail.com>
2018-10-07 00:01:03 +0900

    20181007

--- laravelapp2/routes/web.php
+++ laravelapp2/routes/web.php
@@ -19,7 +19,12 @@ Route::get('hello/add', 'HelloController@add');
 Route::post('hello/add', 'HelloController@create');
 Route::get('hello/edit', 'HelloController@edit');
 Route::post('hello/edit', 'HelloController@update');
-
+Route::get('hello/del', 'HelloController@del');
+Route::post('hello/del', 'HelloController@remove');
+Route::get('hello/show', 'HelloController@show');
+Route::get('person', 'PersonController@index');
+Route::get('person/find', 'PersonController@find');
+Route::post('person/find', 'PersonController@search');
 
 /*
 Route::get('/', function () {
