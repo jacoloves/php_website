29455361c034a27c2d198f30679ff3dc2528d8ca
Shotaro Tanaka <5511068t@gmail.com>
2018-12-11 07:15:50 +0900

    20181211

--- HTML_Design/jquery/script.js
+++ HTML_Design/jquery/script.js
@@ -1,5 +1,10 @@
-$(function() {
-    $('img').fadeOut();
-
-    $('p').slideUp();
-});
+$(function(){
+    $('#language-wrapper').hover(
+        function(){
+            $('.language-text').fadeIn();
+        },
+        function(){
+            $('.language-text').fadeOut();
+        }
+    );
+})
