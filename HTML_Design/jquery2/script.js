a137daec6898414fc3a6a6ba5cc95e2317a88848
Shotaro Tanaka <5511068t@gmail.com>
2018-12-14 06:15:29 +0900

    20181214

--- HTML_Design/jquery2/script.js
+++ HTML_Design/jquery2/script.js
@@ -1,5 +1,23 @@
-$('document').$(function(){
+$(function(){
     $('#login-show').click(function(){
         $('#login-modal').fadeIn();
     });
+    
+    $('.signup-show').click(function(){
+        $('#signup-modal').fadeIn();
+    });
+
+    $('.close-modal').click(function(){
+        $('#login-modal').fadeOut();
+        $('#signup-modal').fadeOut();
+    });
+
+    $('.lesson-hover').hover(
+        function(){
+
+        },
+        function(){
+
+        }
+    );
 });
